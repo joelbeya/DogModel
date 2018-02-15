55dbccebc31a545aaa8e38c80cf1f10b359b3be3
Ibrahima Tounkara <ibrahima.tounkara@etu.umontpellier.fr>
2018-02-15 16:52:02 +0100

    Updating Table Proprietaire

--- dog_odel/dogo/models.py
+++ dog_odel/dogo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+class Article(models.Model):
+    nom = models.TextField(max_length=15)
+    prenom = models.TextField(null=True, max_length=15)
+    dateDeNaissance= models.DateTimeField(default=timezone.now,verbose_name="Date de Naissance")
+    sexe = models.CharField(max_length=1)
+    adresse = models.CharField(max_length=40)
 # Create your models here.
