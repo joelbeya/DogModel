9ffb74b7f4b809bf4001cbf20d58b86a166236cb
Beya Ntumba Joel <joel.beya-ntumba@etu.umontpellier.fr>
2018-02-15 16:32:47 +0100

    classe race à jour

--- dog_odel/dogo/models.py
+++ dog_odel/dogo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class race(models.Model):
+    """docstring for race."""
+    taille = models.IntegerField(max_length = 255)
+    nom_race = models.TextField(max_length = 255)
+    morphologie = models.TextField(max_length = 255)
+    traits_comportementaux = models.TextField(max_length = 255)
+    
+    def __init__(self, arg):
+        super(race, self).__init__()
+        self.arg = arg
