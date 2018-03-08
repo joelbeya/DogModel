cc21770a049e1205df3e4547472c6fe98f1b5f18
Teiki Raihauti <teiki.raihauti@etu.umontpellier.fr>
2018-03-08 18:29:21 +0100

    nouveau url

--- dog_odel/dogo/views.py
+++ dog_odel/dogo/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from .models import *
-from django.http import HttpResponse
 import sys
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Test")
-
-def indexx(request,obj):
-    identifier = getattr(sys.modules[__name__], obj)
-    objets = identifier.all()
+    objets = Chien.objects.all();
     return render(request, 'dogo/index.html', {'objets':objets})
 
+def requete(request,obj):
+    identifier = getattr(sys.modules[__name__], obj)
+    objets = identifier.all()
+    return render(request, 'dogo/requete.html', {'objets':objets})
+
 def show(request, obj, key):
-    objet = obj.find(key)
+    identifier = getattr(sys.modules[__name__], obj)
+    objet = identifier.find(int(key))
     return render(request, 'dogo/show.html', {'objet':objet})
