72cc989ecf0b4278ce3851c5473e5025bbbb6f58
Teiki Raihauti <teiki.raihauti@etu.umontpellier.fr>
2018-03-08 18:55:25 +0100

    formulaire

--- dog_odel/dogo/views.py
+++ dog_odel/dogo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from .forms import *
 import sys
 
 # Create your views here.
@@ -17,3 +18,6 @@ def show(request, obj, key):
     identifier = getattr(sys.modules[__name__], obj)
     objet = identifier.find(int(key))
     return render(request, 'dogo/show.html', {'objet':objet})
+
+def formu(request):
+    return render(request, 'dogo/formu.html', {'form':Subscribe})
