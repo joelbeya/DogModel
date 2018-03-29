5f182d21c15f1a3d89d9d823a2bfb97362cede9d
Teiki Raihauti <teiki.raihauti@etu.umontpellier.fr>
2018-03-29 16:06:57 +0200

    test

--- dog_odel/dogo/forms.py
+++ dog_odel/dogo/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.hashers import *
 from .models import *
 from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
 
 class Subscribe(forms.Form):
     mail = forms.EmailField(label='Your e-mail', max_length=100)
@@ -49,7 +50,7 @@ def log(request):
 
     if check_password(request.POST['password'], m.password):
         request.session['id'] = m.id
-        return HttpResponse("You're logged in." + str(request.session['id']))
+        return redirect('/dogo/index')
     else:
         return HttpResponse("Your username and password didn't match.")
 
@@ -59,4 +60,4 @@ def logout(request):
         del request.session['id']
     except KeyError:
         pass
-    return HttpResponse("You're logged out.")
+    return redirect('/dogo/index')
